af32b69eab1c3dfa997854a4d1f7a0ad9d503428
mathilde renaud <renaud.mathilde95@hotmail.fr>
2024-11-27 16:29:34 +0100

    cards

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,5 +11,6 @@ class Project < ApplicationRecord
       messages: [{ role: "user", content: "Give me a 3 or 4 lines gardening project description for #{name} with the criterias little balcony, sunny all year. Then give me a list of 3 or 4 plants or seeds I would need and the steps to follow for each plant to take car of them (when to water...).Give me only the text of the project, without any of your own answer like 'Here is a simple project'."}]
     })
     return chatgpt_response["choices"][0]["message"]["content"]
+    
   end
 end
