674563741906f08b0e32de0d7907df17f0813465
mathilde renaud <renaud.mathilde95@hotmail.fr>
2024-11-27 14:49:11 +0100

    gpt

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,13 @@ class Project < ApplicationRecord
   has_one_attached :photo
 
   validates :name, :description, :difficulty, :duration, presence: true
+
+  def content
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: "Give me a 3 or 4 lines gardening project description for #{name} with the criterias little balcony, sunny all year. Then give me a list of 3 or 4 plants or seeds I would need and the steps to follow for each plant to take car of them (when to water...).Give me only the text of the project, without any of your own answer like 'Here is a simple project'."}]
+    })
+    return chatgpt_response["choices"][0]["message"]["content"]
+  end
 end
