e8fe1aeaa8655de9c5cf2f4fcd9747dba90d35cc
Victoria Coquet <pupicoquet@hotmail.com>
2024-11-27 11:34:20 +0100

    backend of inputs - done

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,16 @@
 class SpacesController < ApplicationController
+  def new
+    @spaces = Space.all
+  end
+
+  def create
+    @selected_spaces = Space.find(params[:selected_spaces])
+    redirect_to '#'
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:selected_spaces[])
+  end
 end
