15b92d8cb78827acba1255945507d9427446bd9d
Victoria Coquet <pupicoquet@hotmail.com>
2024-11-29 19:52:54 +0100

    xxx

--- app/models/project.rb
+++ app/models/project.rb
@@ -36,9 +36,9 @@ class Project < ApplicationRecord
                   Duration - in either hours, days or weeks.
                   Description - ~80 words.
                   A broken down bulleted list of 1-7 items I would need, put a
-                  '-' between each item.
-                  A numbered list of the method - max 800 words, no headings,
-                  put a '-' between each step.
+                  '|' between each item.
+                  A non-numbered list of the method - max 800 words, no headings,
+                  put a '|' between each step.
                   A fun fact about it - max 20 words.
 
                   When generating the above, generate response in British
@@ -61,8 +61,8 @@ class Project < ApplicationRecord
     new_content = chatgpt_response["choices"][0]["message"]["content"]
 
     split_content = new_content.split('/')
-    # items =
-    # method =
+    items =
+    method = split_content[7]
 
     self.name = split_content[1]
     self.description = split_content[5]
@@ -70,7 +70,7 @@ class Project < ApplicationRecord
     self.duration = split_content[4]
     self.standfirst = split_content[2]
     self.items = split_content[6]
-    self.method = split_content[7]
+    self.method =
     self.fact = split_content[8]
     self.save
 
